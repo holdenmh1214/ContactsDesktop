0b36b214755846404ebd91eec4e234a0650ec0a3
holdenhughes <holdenmh121489@gmail.com>
2015-10-20 14:26:22 -0400

    Complete assignment

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,57 @@
 package sample;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    ObservableList<Contact> items = FXCollections.observableArrayList();
+
+    @FXML
+    TextField nameField;
+
+    @FXML
+    TextField phoneField;
+
+    @FXML
+    TextField emailField;
+
+    @FXML
+    ListView listView;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        listView.setItems(items);
+
+    }
+
+    public void addContact(){
+
+        if (!nameField.getText().equals("") && !phoneField.getText().equals("") && !emailField.getText().equals(""))
+        {
+            Contact contact = new Contact(nameField.getText(), phoneField.getText(), emailField.getText());
+            items.add(contact);
+            nameField.setText("");
+            phoneField.setText("");
+            emailField.setText("");
+        }
+    }
+
+    public void removeContact(){
+
+        Contact oldContact =  (Contact) listView.getSelectionModel().getSelectedItem();
+        items.remove(oldContact);
+        nameField.setText("");
+        phoneField.setText("");
+        emailField.setText("");
+    }
+
 }
